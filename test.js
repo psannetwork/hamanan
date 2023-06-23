6f614d78de85d958b6ba1db6107e007e6acc20e6
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-06-23 17:14:14 +0900

    Update and rename 1.js to test.js

--- test.js
+++ test.js
@@ -41,4 +41,42 @@ for (let i = 0; i < boxedChars.length; i++) {
   const text = boxedChar.textContent.trim();
   recordKey(text);
 }
+//3
+const minDelay = 60;
+const maxDelay = 60;
+
+const keyOverrides = {
+  '\u00A0': ' '    
+};
+
+function getTargetCharacters() {
+  const els = Array.from(document.querySelectorAll('.token span.token_unit'));
+  const chrs = els.map(el => {
+    if (el.firstChild?.classList?.contains('_enter')) {
+      return '\n';
+    }
+    return el.textContent[0];
+  }).map(c => keyOverrides[c] || c); // convert special characters
+  return chrs;
+}
+
+function recordKey(chr) {
+  window.core.record_keydown_time(chr);
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function autoPlay(finish) {
+  const chrs = getTargetCharacters();
+  for (let i = 0; i < chrs.length - (!finish); ++i) {
+    const c = chrs[i];
+    recordKey(c);
+    await sleep(Math.random() * (maxDelay - minDelay) + minDelay);
+  }
+}
+
+autoPlay(true);
+
 
