c8404a76f98a153a75a826103276c7872c6e7c34
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-06-23 17:12:23 +0900

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,3 +1,35 @@
+//1こめ
+const spans = document.querySelectorAll('span[style="opacity: 1;"]');
+const targetTexts = Array.from(spans).map(span => span.textContent.trim());
+
+async function typeText(text) {
+  for (let i = 0; i < text.length; i++) {
+    const char = text[i];
+    recordKey(char);
+    await sleep(Math.random() * (maxDelay - minDelay) + minDelay);
+  }
+}
+
+async function typeTargetTexts() {
+  for (let i = 0; i < targetTexts.length; i++) {
+    const text = targetTexts[i];
+    await typeText(text);
+  }
+}
+
+typeTargetTexts();
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function waitTwoSeconds() {
+  await sleep(2000);
+  console.log('2 seconds have passed.');
+}
+
+waitTwoSeconds();
+//止める
+//2こめ？
 const boxedChars = document.querySelectorAll('.boxed-char');
 
 function recordKey(chr) {
@@ -9,3 +41,4 @@ for (let i = 0; i < boxedChars.length; i++) {
   const text = boxedChar.textContent.trim();
   recordKey(text);
 }
+
