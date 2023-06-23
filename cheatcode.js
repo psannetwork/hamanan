17bf8e1d59a4df1faec8bc8815001104c08e3d4d
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-06-23 17:18:30 +0900

    Update cheatcode.js

--- cheatcode.js
+++ cheatcode.js
@@ -1,46 +1,3 @@
-//1こめ
-const spans = document.querySelectorAll('span[style="opacity: 1;"]');
-const targetTexts = Array.from(spans).map(span => span.textContent.trim());
-
-async function typeText(text) {
-  for (let i = 0; i < text.length; i++) {
-    const char = text[i];
-    recordKey(char);
-    await sleep(Math.random() * (maxDelay - minDelay) + minDelay);
-  }
-}
-
-async function typeTargetTexts() {
-  for (let i = 0; i < targetTexts.length; i++) {
-    const text = targetTexts[i];
-    await typeText(text);
-  }
-}
-
-typeTargetTexts();
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function waitTwoSeconds() {
-  await sleep(2000);
-  console.log('2 seconds have passed.');
-}
-
-waitTwoSeconds();
-//止める
-//2こめ？
-const boxedChars = document.querySelectorAll('.boxed-char');
-
-function recordKey(chr) {
-  window.core.record_keydown_time(chr);
-}
-
-for (let i = 0; i < boxedChars.length; i++) {
-  const boxedChar = boxedChars[i];
-  const text = boxedChar.textContent.trim();
-  recordKey(text);
-}
 //3
 const minDelay = 60;
 const maxDelay = 60;
@@ -56,7 +13,7 @@ function getTargetCharacters() {
       return '\n';
     }
     return el.textContent[0];
-  }).map(c => keyOverrides[c] || c); // convert special characters
+  }).map(c => keyOverrides[c] || c); 
   return chrs;
 }
 
