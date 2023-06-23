164ea2f7519f73e5bf503405c44d4807aa88e203
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-06-24 06:51:20 +0900

    Update dino_cheat.js

--- dino_cheat.js
+++ dino_cheat.js
@@ -1,4 +1,30 @@
-Runner.prototype.gameOver = function(){} //無敵
-Runner.instance_.distanceRan = 12345 / Runner.instance_.distanceMeter.config.COEFFICIENT//スコア
-Runner.instance_.tRex.setJumpVelocity(10)//jump
+function injectCustomCode() {
+  const code = `
+    
+    const score = prompt("スコアを入力してください:");
+    if (score) {
+      const distanceRan = parseFloat(score) / Runner.instance_.distanceMeter.config.COEFFICIENT;
+      Runner.instance_.distanceRan = distanceRan;
+    }
+    
+    const jumpVelocity = prompt("ジャンプ速度を入力してください:");
+    if (jumpVelocity) {
+      Runner.instance_.tRex.setJumpVelocity(parseFloat(jumpVelocity));
+    }
+    
+    const invincibleMode = prompt("無敵モードを有効にしますか？（yes/no）");
+    if (invincibleMode && invincibleMode.toLowerCase() === "yes") {
+      Runner.prototype.gameOver = function() {}; 
+    }
+  `;
+  
+  const script = document.createElement('script');
+  script.textContent = code;
+  document.body.appendChild(script);
+}
 
+function checkPassword() {
+}
+
+checkPassword();
+injectCustomCode();
