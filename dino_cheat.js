a2da058202808864702c55b13e4bccf045de5e43
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-06-23 22:36:57 +0900

    Create dino_cheat.js

---
+++ dino_cheat.js
@@ -0,0 +1,4 @@
+Runner.prototype.gameOver = function(){} //無敵
+Runner.instance_.distanceRan = 12345 / Runner.instance_.distanceMeter.config.COEFFICIENT//スコア
+Runner.instance_.tRex.setJumpVelocity(10)//jump
+
