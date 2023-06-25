a681fe0f95726b1eb1c0775f3e7122948829ae5c
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-06-25 13:58:01 +0900

    Update gui.js

--- gui.js
+++ gui.js
@@ -1,13 +1,15 @@
+// 画像要素の作成
 const square = document.createElement('div');
-square.style.width = '100vw'; // 画面幅いっぱいにする
-square.style.height = '100vh'; // 画面高さいっぱいにする
+square.style.width = '100vw';
+square.style.height = '100vh';
 square.style.backgroundImage = "url('https://sozaino.site/wp-content/uploads/2021/02/utyuu.jpg')";
-square.style.backgroundSize = 'cover'; // 画像を要素に合わせて調整
-square.style.backgroundPosition = 'center'; // 画像を中央に配置
+square.style.backgroundSize = 'cover';
+square.style.backgroundPosition = 'center';
 square.style.position = 'fixed';
 square.style.top = '0px';
 square.style.right = '0px';
-square.style.zIndex = '9990';//四角
+square.style.zIndex = '9990';
+square.style.display = 'none';
 
 // テキスト要素の作成
 const text = document.createElement('div');
@@ -18,6 +20,7 @@ text.style.position = 'fixed';
 text.style.bottom = '0px';
 text.style.left = '0px';
 text.style.zIndex = '9998';
+text.style.display = 'none';
 
 // スライドボタンの作成
 const slideButton = document.createElement('button');
@@ -25,7 +28,7 @@ slideButton.textContent = 'Menu';
 slideButton.style.backgroundColor = 'red';
 slideButton.style.color = 'white';
 slideButton.style.position = 'fixed';
-slideButton.style.bottom = '0px'; // 上から80px下にずらす
+slideButton.style.bottom = '0px';
 slideButton.style.right = '0px';
 slideButton.style.zIndex = '9999';
 
@@ -60,21 +63,21 @@ button1.textContent = 'typing js';
 button1.style.backgroundColor = 'red';
 button1.style.color = 'white';
 button1.style.position = 'fixed';
-button1.style.top = '150px'; // 上から150px下にずらす
+button1.style.top = '150px';
 button1.style.left = '45%';
 button1.style.zIndex = '9999';
-button1.style.display = 'none'; // 初期状態では非表示
-button1.style.width = '150px'; // ボタンの幅を200pxに設定
-button1.style.height = '50px'; // ボタンの高さを50pxに設定
+button1.style.display = 'none';
+button1.style.width = '150px';
+button1.style.height = '50px';
 
 // ボタン1のクリックイベントの追加
 button1.addEventListener('click', () => {
   fetch('https://raw.githubusercontent.com/hirotomoki12345/hamanan/main/cheatcode.js')
-    .then(response => response.text()) // テキストとして取得
+    .then(response => response.text())
     .then(data => {
-      // GitHub上のコードを実行する処理
       console.log('ボタン1がクリックされました');
-      eval(data); // 取得したコードを実行
+      const executeCode = new Function(data);
+      executeCode();
     })
     .catch(error => {
       console.error('エラーが発生しました', error);
@@ -87,21 +90,21 @@ button2.textContent = 'devmode';
 button2.style.backgroundColor = 'green';
 button2.style.color = 'white';
 button2.style.position = 'fixed';
-button2.style.top = '300px'; // 上から200px下にずらす
+button2.style.top = '300px';
 button2.style.left = '45%';
 button2.style.zIndex = '9999';
-button2.style.display = 'none'; // 初期状態では非表示
-button2.style.width = '150px'; // ボタンの幅を200pxに設定
-button2.style.height = '50px'; // ボタンの高さを50pxに設定
+button2.style.display = 'none';
+button2.style.width = '150px';
+button2.style.height = '50px';
 
 // ボタン2のクリックイベントの追加
 button2.addEventListener('click', () => {
   fetch('https://raw.githubusercontent.com/hirotomoki12345/hamanan/main/lately.js')
-    .then(response => response.text()) // テキストとして取得
+    .then(response => response.text())
     .then(data => {
-      // GitHub上のコードを実行する処理
       console.log('ボタン2がクリックされました');
-      eval(data); // 取得したコードを実行
+      const executeCode = new Function(data);
+      executeCode();
     })
     .catch(error => {
       console.error('エラーが発生しました', error);
@@ -114,21 +117,21 @@ button3.textContent = 'dino';
 button3.style.backgroundColor = 'blue';
 button3.style.color = 'white';
 button3.style.position = 'fixed';
-button3.style.top = '450px'; // 上から450px下にずらす
+button3.style.top = '450px';
 button3.style.left = '45%';
 button3.style.zIndex = '9999';
-button3.style.display = 'none'; // 初期状態では非表示
-button3.style.width = '150px'; // ボタンの幅を150pxに設定
-button3.style.height = '50px'; // ボタンの高さを50pxに設定
+button3.style.display = 'none';
+button3.style.width = '150px';
+button3.style.height = '50px';
 
 // ボタン3のクリックイベントの追加
 button3.addEventListener('click', () => {
   fetch('https://raw.githubusercontent.com/hirotomoki12345/hamanan/main/dino_cheat.js')
-    .then(response => response.text()) // テキストとして取得
+    .then(response => response.text())
     .then(data => {
-      // GitHub上のコードを実行する処理
       console.log('ボタン3がクリックされました');
-      eval(data); // 取得したコードを実行
+      const executeCode = new Function(data);
+      executeCode();
     })
     .catch(error => {
       console.error('エラーが発生しました', error);
