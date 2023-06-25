445421caa23666b24f285766f41eb2cccf4a0498
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-06-25 15:32:32 +0900

    Update gui.js

--- gui.js
+++ gui.js
@@ -55,6 +55,7 @@ slideButton.addEventListener('click', () => {
     button1.style.display = 'block';
     button2.style.display = 'block';
     button3.style.display = 'block';
+    button4.style.display = 'block';
     slideButton.textContent = 'Close';
   } else {
     // 表示されている場合は非表示にする
@@ -63,6 +64,7 @@ slideButton.addEventListener('click', () => {
     button1.style.display = 'none';
     button2.style.display = 'none';
     button3.style.display = 'none';
+    button4.style.display = 'none';
     slideButton.textContent = 'Menu';
   }
 });
@@ -73,8 +75,8 @@ button1.textContent = 'typing js';
 button1.style.backgroundColor = 'red';
 button1.style.color = 'white';
 button1.style.position = 'fixed';
-button1.style.top = '150px';
-button1.style.left = '45%';
+button1.style.top = '50%';
+button1.style.left = 'calc(50% - 75px)';
 button1.style.zIndex = '9999';
 button1.style.display = 'none';
 button1.style.width = '150px';
@@ -93,14 +95,15 @@ button1.addEventListener('click', () => {
       console.error('エラーが発生しました', error);
     });
 });
+
 // ボタン要素2の作成
 const button2 = document.createElement('button');
 button2.textContent = 'devmode';
 button2.style.backgroundColor = 'green';
 button2.style.color = 'white';
 button2.style.position = 'fixed';
-button2.style.top = '300px';
-button2.style.left = '45%';
+button2.style.top = 'calc(50% + 70px)';
+button2.style.left = 'calc(50% - 75px)';
 button2.style.zIndex = '9999';
 button2.style.display = 'none';
 button2.style.width = '150px';
@@ -120,15 +123,14 @@ button2.addEventListener('click', () => {
     });
 });
 
-
 // ボタン要素3の作成
 const button3 = document.createElement('button');
 button3.textContent = 'dino';
 button3.style.backgroundColor = 'blue';
 button3.style.color = 'white';
 button3.style.position = 'fixed';
-button3.style.top = '450px';
-button3.style.left = '45%';
+button3.style.top = 'calc(50% + 190px)';
+button3.style.left = 'calc(50% - 75px)';
 button3.style.zIndex = '9999';
 button3.style.display = 'none';
 button3.style.width = '150px';
@@ -148,11 +150,42 @@ button3.addEventListener('click', () => {
     });
 });
 
+// ボタン要素4の作成
+const button4 = document.createElement('button');
+button4.textContent = 'Stop Timers';
+button4.style.backgroundColor = 'orange';
+button4.style.color = 'white';
+button4.style.position = 'fixed';
+button4.style.top = 'calc(50% + 310px)';
+button4.style.left = 'calc(50% - 75px)';
+button4.style.zIndex = '9999';
+button4.style.display = 'none';
+button4.style.width = '150px';
+button4.style.height = '50px';
+
+// ボタン4のクリックイベントの追加
+button4.addEventListener('click', () => {
+  // 実行中のすべてのタイマーをクリア
+  let timeoutId = window.setTimeout(null, 0);
+  while (timeoutId >= 0) {
+    window.clearTimeout(timeoutId);
+    timeoutId--;
+  }
+
+  // 実行中のすべてのインターバルをクリア
+  let intervalId = window.setInterval(null, 0);
+  while (intervalId >= 0) {
+    window.clearInterval(intervalId);
+    intervalId--;
+  }
+});
+
 // ボタンをドキュメントに追加
 document.body.appendChild(slideButton);
 document.body.appendChild(button1);
 document.body.appendChild(button2);
 document.body.appendChild(button3);
+document.body.appendChild(button4);
 
 // テキストをドキュメントに追加
 document.body.appendChild(text);
