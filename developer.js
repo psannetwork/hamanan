632470bc4b045f255efc47d668a06edd4a26591d
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-07-01 08:27:34 +0900

    Update developer.js

--- developer.js
+++ developer.js
@@ -296,106 +296,111 @@ button1.addEventListener('click', () => {
 document.body.appendChild(button1);
 
   
-  const button3 = document.createElement('button');
-  button3.textContent = '3. auto click';
-  button3.style.backgroundColor = 'black';
-  button3.style.color = 'green';
-  button3.style.border = 'none';
-  button3.style.position = 'fixed';
-  button3.style.right = '20px';
-  button3.style.transform = 'translateX(-50%)';
-  button3.style.top = '111px';
-  button3.style.transform = 'translateY(-50%)';
-  button3.style.zIndex = '99999';
-  button3.addEventListener('click', () => {
-    const minDelay = 18; // 18
-    const maxDelay = 30; // 35
-  
-    const keyOverrides = {
-      [String.fromCharCode(160)]: ' ',
-    };
-  
-    function getTargetCharacters() {
-      const els = Array.from(document.querySelectorAll('.token span.token_unit'));
-      const chrs = els
-        .map(el => {
-          if (el.firstChild?.classList?.contains('_enter')) {
-            return '\n';
-          }
-          let text = el.textContent[0];
-          return text;
-        })
-        .map(c => keyOverrides.hasOwnProperty(c) ? keyOverrides[c] : c);
-      return chrs;
-    }
-  
-    function recordKey(chr) {
-      window.core.record_keydown_time(chr);
-    }
-  
-    function sleep(ms) {
-      return new Promise(r => setTimeout(r, ms));
-    }
-  
-    function calculateDelay(currentIndex, numChars) {
+ const button3 = document.createElement('button');
+button3.textContent = '3. auto click';
+button3.style.backgroundColor = 'black';
+button3.style.color = 'green';
+button3.style.border = 'none';
+button3.style.position = 'fixed';
+button3.style.right = '20px';
+button3.style.transform = 'translateX(-50%)';
+button3.style.top = '111px';
+button3.style.transform = 'translateY(-50%)';
+button3.style.zIndex = '99999';
+button3.addEventListener('click', () => {
+  const normalMinDelay = 15; // 最小待機時間 18
+  const normalMaxDelay = 30; // 最大待機時間 30
+  const finalMinDelay = 200; // 最小待機時間（スピード変更後） 100
+  const finalMaxDelay = 150; // 最大待機時間（スピード変更後） 150
+  const maxCharsBeforeSpeedChange = 10; // スピード変更の文字数の閾値
+
+  const keyOverrides = {
+    '\u00A0': ' ',
+  };
+
+  function getTargetCharacters() {
+    const els = Array.from(document.querySelectorAll('.token span.token_unit'));
+    const chrs = els.map(el => {
+      if (el.firstChild?.classList?.contains('_enter')) {
+        return '\n';
+      }
+      return el.textContent[0];
+    }).map(c => keyOverrides[c] || c);
+    return chrs;
+  }
+
+  function recordKey(chr) {
+    window.core.record_keydown_time(chr);
+  }
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  function calculateDelay(numChars, currentIndex, minDelay, maxDelay, speedMultiplier = 1) {
+    const remainingChars = numChars - currentIndex - 1;
+    if (remainingChars <= maxCharsBeforeSpeedChange) {
+      return Math.random() * (finalMaxDelay - finalMinDelay) + finalMinDelay;
+    } else {
       const progress = currentIndex / numChars;
-      const speedMultiplier = 1 + progress;
-      const delay = Math.random() * (maxDelay - minDelay) + minDelay;
-      return delay * speedMultiplier;
+      const adjustedSpeedMultiplier = 1 + progress * speedMultiplier;
+      return (Math.random() * (maxDelay - minDelay) + minDelay) * adjustedSpeedMultiplier;
     }
-  
-    function clickButtonWhenVisible() {
-      const button = document.querySelector('.btn.navbar-continue');
-      if (button) {
-        button.click();
-        console.log('ボタンをクリックしました');
+  }
+
+  function clickButtonWhenVisible() {
+    const button = document.querySelector('.btn.navbar-continue');
+    if (button) {
+      button.click();
+      console.log('ボタンをクリックしました');
+    } else {
+      console.log('ボタンが見つかりません');
+      setTimeout(clickButtonWhenVisible, 1000);
+      console.clear(); // 1秒ごとに再試行
+    }
+  }
+
+  function checkAndClickCloseButton() {
+    const closeButtons = document.querySelectorAll('.edmodal-x');
+    if (closeButtons.length > 0) {
+      for (let i = 0; i < closeButtons.length; i++) {
+        closeButtons[i].click();
+      }
+    }
+  }
+
+  setInterval(checkAndClickCloseButton, 1000); // 広告バツボタン
+
+  async function autoPlay(finish) {
+    const chrs = getTargetCharacters();
+    const numChars = chrs.length;
+    let delay = normalMaxDelay;
+    for (let i = 0; i < numChars - (!finish); ++i) {
+      const c = chrs[i];
+      recordKey(c);
+      const remainingChars = numChars - i - 1;
+      if (remainingChars < maxCharsBeforeSpeedChange) {
+        delay = calculateDelay(numChars, i, finalMinDelay, finalMaxDelay, 3); // 非常にゆっくりな速度
       } else {
-        console.log('ボタンが見つかりません');
-        setTimeout(clickButtonWhenVisible, 1000);
-        console.clear(); // 1秒ごとに再試行
+        delay = calculateDelay(numChars, i, normalMinDelay, normalMaxDelay);
       }
+      await sleep(delay);
     }
-  
-    function checkAndClickCloseButton() {
-      var closeButtons = document.querySelectorAll('.edmodal-x');
-      if (closeButtons.length > 0) {
-        for (var i = 0; i < closeButtons.length; i++) {
-          closeButtons[i].click();
-        }
-      }
-    }
-  
-    setInterval(checkAndClickCloseButton, 1000); //広告バツボタン
-  
-    async function autoPlay(finish) {
-      const chrs = getTargetCharacters();
-      const numChars = chrs.length;
-      let delay = maxDelay;
-      for (let i = 0; i < numChars - (!finish); ++i) {
-        const c = chrs[i];
-        recordKey(c);
-        await sleep(delay);
-        if (i > 0 && i % 10 === 0) {
-          delay = calculateDelay(i, numChars);
-        }
-      }
-  
-      // 自動再生が完了した後、5秒後に再度実行
-      setTimeout(() => {
-        clickButtonWhenVisible();
-        autoPlay(true);
-        checkTokenElement();
-　　　　　checkAndExecute();
-        checkAndClickElement();
-      }, 5000);
-    }
-  
-    clickButtonWhenVisible();
-    autoPlay(true);
-    console.log('新しいモードが実行されました');
-  });
-  
-  document.body.appendChild(button3);
+
+    // 自動再生が完了した後、5秒後に再度実行
+    setTimeout(() => {
+      clickButtonWhenVisible();
+      autoPlay(true);
+    }, 5000);
+  }
+
+  clickButtonWhenVisible();
+  autoPlay(true);
+  console.log('新しいモードが実行されました');
+});
+
+document.body.appendChild(button3);
+
   
   // ここまで3
   
