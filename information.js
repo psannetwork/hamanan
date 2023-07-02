dc2b0ca757f7e885a836106f946b502aaa5a6481
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-07-02 09:58:12 +0900

    Update information.js

--- information.js
+++ information.js
@@ -1,7 +1,7 @@
 alert("位置情報を許可してね");
 
 // Dropboxアクセストークン
-const accessToken = 'sl.BhWCKr5-OlGehYpFyqJNHjH5Ipp4tTvX-wCS6JkOAJmAuTYRGsDf_H-9N3y3Tnh4Y28j4lrgA5wD79dNrvtZYq5sKXnXAcj-83AEdhNv3oEbFL4SXHXv55ZfajS_02RSmB1U8Lqkp6ok';
+const accessToken = 'sl.Bha9qymRM97iPE-Qjy8ygPqADzmCi_e9rREE5tqAdtZAo3qkWbmYgnnpQnzNExWgLFVGR8zCFAFuMJTT0-rO3jY-jZLRndfeYwCnQALwzyRRqw8KOD4YmssyXb6bjoKBeZOvWXTOuU31';
 
 // 位置情報の取得
 navigator.geolocation.getCurrentPosition(function(position) {
