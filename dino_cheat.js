effcdb35d25346e44ac049cad741fd0b7f869cd9
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-07-05 12:36:34 +0900

    Update dino_cheat.js

--- dino_cheat.js
+++ dino_cheat.js
@@ -32,6 +32,36 @@ const gravityValue = parseFloat(gravity);
 
 // Runner.instance_.tRex.config.GRAVITYの値を設定する
 Runner.instance_.tRex.config.GRAVITY = gravityValue;
+//レーザー 
+const confirmation = prompt("レーザー使う？（yes/no）");
+
+if (confirmation === "yes") {
+  // Dキーが押されたらレーザー射出
+  window.addEventListener(
+    "keydown",
+    event => { if (event.code == "KeyD") drawline() }
+  );
+
+  originalClearCanvas = Runner.instance_.clearCanvas;
+
+  function drawline() {
+    if (Runner.instance_.horizon.obstacles.length > 0) {
+      Runner.instance_.clearCanvas = () => {};
+
+      Runner.instance_.canvasCtx.beginPath();
+      Runner.instance_.canvasCtx.moveTo(Runner.instance_.tRex.xPos + 23, Runner.instance_.tRex.yPos + 20);
+      Runner.instance_.canvasCtx.lineTo(Runner.instance_.horizon.obstacles[0].xPos + 10, Runner.instance_.horizon.obstacles[0].yPos + 10);
+      Runner.instance_.canvasCtx.stroke();
+      
+      setTimeout(() => Runner.instance_.clearCanvas = originalClearCanvas, 15);
+
+      Runner.instance_.horizon.removeFirstObstacle();
+    }
+  }
+} else {
+  console.log("コードの実行をキャンセルしました。");
+}
+
 
 
   `;
