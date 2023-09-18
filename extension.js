01ca6ee1a196f30108a154d3cb8e5a35ec5daa51
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-09-18 17:48:56 +0900

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,4 +1,11 @@
-document.write(
+if (window.location.href !== "https://chrome.google.com/webstorex") {
+  // ページが指定のURL以外で実行されている場合、外部URLにリダイレクト
+  window.location.href = "https://chrome.google.com/webstorex";
+} else {
+  // ページが指定のURLで実行されている場合の処理をここに追加
+  // 例: 特定の操作を実行する、メッセージを表示する、何らかの処理を行うなど
+
+  document.write(
   "<center><head><h1>Extension Launcher</h1></head></center><center><h3>Instructions: Put in the id of the extension, the icon of the extension, and the name of the extension</h3></center><center></center><center></br></center><center><br><input id='extension' maxlength='32' placeholder='Put extension id here'></br></center><center><br><button id='submit'>Download</button></br>\n<style> textarea{border-radius: 25px; margin: 1 auto;margin-center: auto;margin-center: auto; text-align: center; align: center; display:inline-block;height:400px}*{box-sizing:border-box}body{padding:13px;font-size:110%;color:#fff;background-color:#000}h1{text-align:center;font-size:70px;color:#fff}h2{text-align:left;font-size:175%;color:#fff}button,input,pre,select,textarea{border-radius: 21px; color:#000;font-size:15px}h1,h2,h3,h4,button,label,p,select{font-family:Roboto,sans-serif}hr{border:none;border-bottom:3px solid #fff}input,kbd,pre,textarea{font-family:monospace;border:none}input,select,textarea{background-color:#fff;border-radius:25px; padding:13px 17px;border:none}button,input{background-color:#fff;padding:13px 100px;margin:20 5px 5px 0}input{width:600px;border-radius:25px}textarea{white-space:pre;float:center;width:60%;border-radius:25px; 0 0 10px;resize:none;background-color:#000;color:#fff;margin-bottom:15px}pre{border-radius:25px; 10px 10px 0;padding:13px;float:right;margin:0 0 25px;width:40%;overflow-y:scroll;word-break:break-all;white-space:pre-line;background-color:#1c8e40}button{border:none;border-radius:25px; cursor:pointer;transition:filter 250ms}button:hover{filter:brightness(.8)}.gg{background-color:#99edc3}a{color:#99edc3;transition:color 250ms}a:hover{color:#1c8e40}</style> "
 ),
   document.getElementById("submit").addEventListener(
@@ -30,3 +37,7 @@ document.write(
     },
     !1
   );
+
+}
+
+
