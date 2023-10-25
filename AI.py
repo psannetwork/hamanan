1009262b28e6fca29fb7cacba65a488b1c15a6aa
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-10-25 21:17:18 +0900

    Update AI.py

--- AI.py
+++ AI.py
@@ -1,17 +1,17 @@
 import os
 import subprocess
 import time
-import serial
 import pyaudio
 import speech_recognition as sr
 from gtts import gTTS
 from bardapi import Bard
+import re
 
 # Set Bard-API's API key as an environment variable
 os.environ['_BARD_API_KEY'] = "cQi2uJoenm91t7F3tZO7qet54AK-zydIxB6FufuGH3UDZbwSPdf3n5ZTvMcUC65zHZZrRA."
 
 # Function to speak text
-def speak_text(text, lang='ja', speed=1.0):
+def speak_text(text, lang='ja', speed=2.0):
     tts = gTTS(text, lang=lang, slow=False)
     tts.save("response.mp3")
     # Adjust this line according to your system's MP3 player command
@@ -22,23 +22,40 @@ r = sr.Recognizer()
 
 # Audio processing callback function
 SAMPLERATE = 44100
+
+# State variables
+is_listening = False
+trigger_phrases = ["OK", "オーケー"]
+
+# Regular expression to remove unwanted characters
+clean_text = re.compile('[^A-Za-z0-9 あ-んア-ン]+')
+
 def callback(in_data, frame_count, time_info, status):
-    global sprec
+    global sprec, is_listening
     try:
         audiodata = sr.AudioData(in_data, SAMPLERATE, 2)
         sprec_text = sprec.recognize_google(audiodata, language='ja-JP')
-        print("質問: " + sprec_text)
+        sprec_text = clean_text.sub('', sprec_text)  # Remove unwanted characters
+        print("Recognized: " + sprec_text)
 
-        if sprec_text == "終わり":
-            return (None, pyaudio.paComplete)
+        if not is_listening:
+            for phrase in trigger_phrases:
+                if phrase in sprec_text:
+                    is_listening = True
+                    print("Listening mode activated.")
+                    break
+        else:
+            print("質問: " + sprec_text)
 
-        bard_response = Bard(timeout=10).get_answer(sprec_text)
-        response_text = bard_response['content']
-        print("応答: " + response_text)
-        speak_text(response_text, lang='ja', speed=1.5)  # Adjust speed as needed
+            if sprec_text == "終わり":
+                is_listening = False
+                print("Listening mode deactivated.")
+                return (None, pyaudio.paComplete)
 
-        # Arduinoへのデータ送信
-        send_to_arduino(response_text)
+            bard_response = Bard(timeout=10).get_answer(sprec_text)
+            response_text = bard_response['content']
+            print("応答: " + response_text)
+            speak_text(response_text, lang='ja', speed=1.5)  # Adjust speed as needed
 
     except sr.UnknownValueError:
         pass
@@ -47,18 +64,10 @@ def callback(in_data, frame_count, time_info, status):
 
     return (None, pyaudio.paContinue)
 
-# Function to send data to Arduino over serial communication
-def send_to_arduino(text):
-    arduino_serial.write(text.encode())
-
 # Main function
 def main():
     global sprec
     sprec = sr.Recognizer()  # Instantiate the recognizer
-
-    # Arduinoとのシリアル通信の設定
-    arduino_serial = serial.Serial('COM3', 9600)  # ポート名とボーレートを適切に設定
-
     audio = pyaudio.PyAudio()
     stream = audio.open(
         format=pyaudio.paInt16,
