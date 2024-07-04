180d8b87c480ad70fbc391ceb9281152c81fe188
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-07-05 07:26:23 +0900

    Create typing.js

---
+++ typing/typing.js
@@ -0,0 +1,361 @@
+function clickContinuously() {
+  const continueButton = document.querySelector('.btn.btn-lg.pull-right.btn-primary');
+  if (continueButton) {
+    continueButton.click();
+  }
+}
+
+setInterval(clickContinuously, 1000);
+
+function clickElement() {
+  var element = document.querySelector('.nav-icon');
+  var excludedElement1 = document.querySelector('.boxed-line');
+  var excludedElement2 = document.querySelector('.token');
+
+  if (element && !(excludedElement1 || excludedElement2)) {
+    var event = new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      view: window
+    });
+    element.dispatchEvent(event);
+  }
+}
+
+setInterval(clickElement, 1000);
+
+function checkAndExecute() {
+  const boxedLineElements = document.getElementsByClassName('boxed-line');
+  if (boxedLineElements.length > 0) {
+    const boxedChars = document.querySelectorAll('.boxed-char');
+
+    async function typeCharacters() {
+      for (const boxedChar of boxedChars) {
+        const text = boxedChar.textContent.trim();
+        await new Promise(resolve => {
+          window.core.record_keydown_time(text);
+          setTimeout(resolve, 50);
+        });
+      }
+    }
+
+    typeCharacters();
+  }
+}
+
+const intervalId = setInterval(checkAndExecute, 1000);
+
+function checkTokenElement() {
+  const tokenElement = document.querySelector('.token span.token_unit');
+  const boxedLineElement = document.querySelector('.boxed-line');
+
+  if (!tokenElement && !boxedLineElement) {
+    console.log('.token span.token_unit 要素も boxed-line 要素も存在しないため、navID を実行します');
+    navID();
+  } else {
+    console.log('.token span.token_unit 要素か boxed-line 要素の少なくとも一方が存在します');
+    setTimeout(checkTokenElement, 1000);
+  }
+}
+
+function autorope() {
+  checkTokenElement();
+  console.log('関数が実行されました');
+}
+
+setInterval(autorope, 3000);
+
+function disableButton(button) {
+  button.disabled = true;
+  button.style.backgroundColor = 'grey';
+}
+
+const button1 = document.createElement('button');
+button1.textContent = '1. auto on';
+button1.style.backgroundColor = 'red';
+button1.style.color = 'blue';
+button1.style.border = 'none';
+button1.style.position = 'fixed';
+button1.style.right = '18px';
+button1.style.top = '52px';
+button1.style.zIndex = '99999';
+button1.addEventListener('click', () => {
+  disableButton(button1);
+  
+  function clickBoxWithZeroStars() {
+    const boxElements = document.querySelectorAll('.box');
+    const targetBoxElements = Array.from(boxElements).filter((boxElement) => {
+      const starsElement = boxElement.querySelector('.stars.stars-0');
+      return starsElement !== null;
+    });
+
+    if (targetBoxElements.length > 0) {
+      const randomIndex = Math.floor(Math.random() * targetBoxElements.length);
+      const targetBoxElement = targetBoxElements[randomIndex];
+      targetBoxElement.click();
+    }
+
+    setTimeout(clickBoxWithZeroStars, 1000);
+  }
+
+  clickBoxWithZeroStars();
+});
+
+document.body.appendChild(button1);
+
+const button3 = document.createElement('button');
+button3.textContent = '3. auto click';
+button3.style.backgroundColor = 'yellow';
+button3.style.color = 'red';
+button3.style.border = 'none';
+button3.style.position = 'fixed';
+button3.style.right = '20px';
+button3.style.transform = 'translateX(-50%)';
+button3.style.top = '111px';
+button3.style.transform = 'translateY(-50%)';
+button3.style.zIndex = '99999';
+button3.addEventListener('click', () => {
+  disableButton(button3);
+  
+  const normalMinDelay = 15;
+  const normalMaxDelay = 30;
+  const finalMinDelay = 200;
+  const finalMaxDelay = 150;
+  const maxCharsBeforeSpeedChange = 10;
+
+  const keyOverrides = {
+    '\u00A0': ' ',
+  };
+
+  function getTargetCharacters() {
+    const els = Array.from(document.querySelectorAll('.token span.token_unit'));
+    const chrs = els.map(el => {
+      if (el.firstChild?.classList?.contains('_enter')) {
+        return '\n';
+      } else if (el.firstChild?.classList?.contains('_tab')) {
+        return '\t';
+      }
+      return el.textContent[0];
+    }).map(c => keyOverrides[c] || c);
+    return chrs;
+  }
+
+  function recordKey(chr) {
+    window.core.record_keydown_time(chr);
+  }
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  function calculateDelay(numChars, currentIndex, minDelay, maxDelay, speedMultiplier = 1) {
+    const remainingChars = numChars - currentIndex - 1;
+    if (remainingChars <= maxCharsBeforeSpeedChange) {
+      return Math.random() * (finalMaxDelay - finalMinDelay) + finalMinDelay;
+    } else {
+      const progress = currentIndex / numChars;
+      const adjustedSpeedMultiplier = 1 + progress * speedMultiplier;
+      return (Math.random() * (maxDelay - minDelay) + minDelay) * adjustedSpeedMultiplier;
+    }
+  }
+
+  function clickButtonWhenVisible() {
+    const button = document.querySelector('.btn.navbar-continue');
+    if (button) {
+      button.click();
+      console.log('ボタンをクリックしました');
+    } else {
+      console.log('ボタンが見つかりません');
+      setTimeout(clickButtonWhenVisible, 1000);
+      console.clear();
+    }
+  }
+
+  function checkAndClickCloseButton() {
+    const closeButtons = document.querySelectorAll('.edmodal-x');
+    if (closeButtons.length > 0) {
+      for (let i = 0; i < closeButtons.length; i++) {
+        closeButtons[i].click();
+      }
+    }
+  }
+
+  setInterval(checkAndClickCloseButton, 1000);
+
+  async function autoPlay(finish) {
+    const chrs = getTargetCharacters();
+    const numChars = chrs.length;
+    let delay = normalMaxDelay;
+    for (let i = 0; i < numChars - (!finish); ++i) {
+      const c = chrs[i];
+      recordKey(c);
+      const remainingChars = numChars - i - 1;
+      if (remainingChars < maxCharsBeforeSpeedChange) {
+        delay = calculateDelay(numChars, i, finalMinDelay, finalMaxDelay, 3);
+      } else {
+        delay = calculateDelay(numChars, i, normalMinDelay, normalMaxDelay);
+      }
+      await sleep(delay);
+    }
+
+    setTimeout(() => {
+      clickButtonWhenVisible();
+      autoPlay(true);
+    }, 5000);
+  }
+
+  clickButtonWhenVisible();
+  autoPlay(true);
+  console.log('新しいモードが実行されました');
+});
+
+document.body.appendChild(button3);
+
+const button2 = document.createElement('button');
+button2.textContent = '2. miss click';
+button2.style.backgroundColor = 'green';
+button2.style.color = 'white';
+button2.style.border = 'none';
+button2.style.position = 'fixed';
+button2.style.right = '20px';
+button2.style.transform = 'translateX(-50%)';
+button2.style.top = '88px';
+button2.style.transform = 'translateY(-50%)';
+button2.style.zIndex = '99999';
+button2.addEventListener('click', () => {
+  disableButton(button2);
+
+  const normalMinDelay = 15;
+  const normalMaxDelay = 30;
+  const finalMinDelay = 200;
+  const finalMaxDelay = 150;
+  const maxCharsBeforeSpeedChange = 10;
+  const typoChance = 0.09;
+
+  const keyOverrides = {
+    '\u00A0': ' ',
+  };
+
+  function getTargetCharacters() {
+    const els = Array.from(document.querySelectorAll('.token span.token_unit'));
+    const chrs = els.map(el => {
+      if (el.firstChild?.classList?.contains('_enter')) {
+        return '\n';
+      } else if (el.firstChild?.classList?.contains('_tab')) {
+        return '\t';
+      }
+      return el.textContent[0];
+    }).map(c => keyOverrides[c] || c);
+    return chrs;
+  }
+
+  function recordKey(chr) {
+    if (chr === '\t') {
+      const event = new KeyboardEvent('keydown', {
+        key: 'Tab',
+        code: 'Tab',
+        keyCode: 9,
+        bubbles: true,
+        cancelable: true,
+      });
+      document.dispatchEvent(event);
+    } else {
+      window.core.record_keydown_time(chr);
+    }
+  }
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  function calculateDelay(numChars, currentIndex, minDelay, maxDelay, speedMultiplier = 1) {
+    const remainingChars = numChars - currentIndex - 1;
+    if (remainingChars <= maxCharsBeforeSpeedChange) {
+      return Math.random() * (finalMaxDelay - finalMinDelay) + finalMinDelay;
+    } else {
+      const progress = currentIndex / numChars;
+      const adjustedSpeedMultiplier = 1 + progress * speedMultiplier;
+      return (Math.random() * (maxDelay - minDelay) + minDelay) * adjustedSpeedMultiplier;
+    }
+  }
+
+  function clickButtonWhenVisible() {
+    const button = document.querySelector('.btn.navbar-continue');
+    if (button) {
+      button.click();
+      console.log('ボタンをクリックしました');
+    } else {
+      console.log('ボタンが見つかりません');
+      setTimeout(clickButtonWhenVisible, 1000);
+      console.clear();
+    }
+  }
+
+  function checkAndClickCloseButton() {
+    const closeButtons = document.querySelectorAll('.edmodal-x');
+    if (closeButtons.length > 0) {
+      for (let i = 0; i < closeButtons.length; i++) {
+        closeButtons[i].click();
+      }
+    }
+  }
+
+  setInterval(checkAndClickCloseButton, 1000);
+
+  async function autoPlay(finish) {
+    const chrs = getTargetCharacters();
+    const numChars = chrs.length;
+    let delay = normalMaxDelay;
+    for (let i = 0; i < numChars - (!finish); ++i) {
+      const c = chrs[i];
+      recordKey(c);
+      const remainingChars = numChars - i - 1;
+      if (remainingChars < maxCharsBeforeSpeedChange) {
+        delay = calculateDelay(numChars, i, finalMinDelay, finalMaxDelay, 3);
+      } else {
+        delay = calculateDelay(numChars, i, normalMinDelay, normalMaxDelay);
+      }
+      await sleep(delay);
+
+      if (chrs[i + 1] && chrs[i + 1] !== ' ' && Math.random() < typoChance) {
+        await handleTypo(i);
+      }
+    }
+
+    setTimeout(() => {
+      clickButtonWhenVisible();
+      autoPlay(true);
+    }, 5000);
+  }
+
+  async function handleTypo(currentIndex) {
+    const chrs = getTargetCharacters();
+    const nextChar = chrs[currentIndex + 1];
+    if (nextChar && nextChar !== ' ') {
+      await sleep(50);
+      recordKey(getRandomTypoCharacter(nextChar));
+    }
+  }
+
+  function getRandomTypoCharacter(char) {
+    const possibleChars = 'abcdefghijklmnopqrstuvwxyz';
+    const randomChar = possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+    return randomChar !== char ? randomChar : getRandomTypoCharacter(char);
+  }
+
+  clickButtonWhenVisible();
+  autoPlay(true);
+  console.log('新しいモードが実行されました');
+});
+
+document.body.appendChild(button2);
+
+const container = document.createElement('div');
+container.style.position = 'fixed';
+container.style.top = '10px';
+container.style.right = '10px';
+container.appendChild(button1);
+container.appendChild(button2);
+
+document.body.appendChild(container);
+container.style.zIndex = '99999';
